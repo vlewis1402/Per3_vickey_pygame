b531b262d61cb6f9b1641f4c3f62bad179f11a93
vlewis1402 <vickeylewis15@gmail.com>
2018-05-11 17:43:42 +0000

    Initial commit

---
+++ gpio_button.py
@@ -0,0 +1,4 @@
+from gpiozero import Button
+button = Button(2)
+button.wait_for_press()
+print("Never gonna give you up never gonna let you down never gonna run around and desert you never gonna make you cry never gonna say goodbye never gonna tell a lie and hurt you")
