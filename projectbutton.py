4ca02c04936e6eb3797e61fe6befec2b6997cd53
vlewis1402 <vickeylewis15@gmail.com>
2018-06-05 18:08:16 +0000

    Updated number of buttons

--- projectbutton.py
+++ projectbutton.py
@@ -1,12 +1,21 @@
 from gpiozero import Button
-button = Button(2)
-button2 = Button(3)
-button.wait_for_press()
-button2.wait_for_press()
-print("Never gonna give you up ")
+left_button = Button(2)
+right_button = Button(3)
+
+
 
 #What I need to do:
 #- code it so that buttons give a true/false input depending on if they're being pushed
 #- then they have to return whatever direction they're being pushed in
 #- then we have to code Nik's game to recognize that and move with it
 
+def move(left, right):
+    if left.is_pressed:
+        print("Left")
+
+    if right.is_pressed:
+         print("Right")
+
+
+while True:
+    move(left_button, right_button)
